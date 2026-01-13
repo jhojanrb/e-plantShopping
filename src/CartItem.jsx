cef093b491413c5c9fd8e0699abf4895b9282fc6
jhojan4523 <jhojan4523@gmail.com>
2026-01-12 21:10:04 -0500

    tarea 3

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,37 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => {
+    const price = parseFloat(item.cost.replace('$', ''));
+    return total + price * item.quantity;
+  }, 0);
   };
 
   const handleContinueShopping = (e) => {
-   
+   alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({
+    name: item.name,
+    quantity: item.quantity + 1
+  }));
+
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1){
+        dispatch(updateQuantity({
+        name: item.name,
+        quantity: item.quantity - 1
+        }));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
